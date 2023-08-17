4665d39fc37ec4a3682e43291643f899f4f4a9b4
gh0sshx <ghoss@tuta.io>
2023-08-17 17:43:15 -0400

    customize

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Configure my new machine
 
     sudo apt update && sudo apt upgrade -y
+>
+    chmod +x && ./decompress_and_move.sh
---
+++ decompress_and_move.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo -e "[+] creando directorios"
+mkdir $HOME/.themes >/dev/null
+mkdir $HOME/.icons >/dev/null
+
+echo -e "[+] Descomprimiendo"
+unzip customize.zip -d $HOME/Documents >/dev/null
+cd $HOME/Documents/customize >/dev/null
+
+
+mkdir -p $HOME/Documents/customize/Colloid && tar -xvf Colloid.tar.xz -C $HOME/Documents/customize/Colloid >/dev/null
+cp -r $HOME/Documents/customize/Colloid/* $HOME/.themes/ >/dev/null
+
+mkdir -p $HOME/Documents/customize/Marble-shell && tar -xvf Marble-shell.tar.xz -C $HOME/Documents/customize/Marble-shell >/dev/null
+cp -r $HOME/Documents/customize/Marble-shell/* $HOME/.themes/ >/dev/null
+
+mkdir -p $HOME/Documents/customize/Orchis-Grey && tar -xvf Orchis-Grey.tar.xz -C $HOME/Documents/customize/Orchis-Grey >/dev/null
+cp -r $HOME/Documents/customize/Orchis-Grey/* $HOME/.themes/ >/dev/null
+
+mkdir -p $HOME/Documents/customize/Orchis-Teal && tar -xvf Orchis-Teal.tar.xz -C $HOME/Documents/customize/Orchis-Teal >/dev/null
+cp -r $HOME/Documents/customize/Orchis-Teal/* $HOME/.themes/ >/dev/null
+
+unzip kora-grey-1-6-0.zip >/dev/null
+cp -r $HOME/Documents/customize/kora_grey/* $HOME/.icons/ >/dev/null
+
+echo -e "[+] Cerrando sesión"
+cp -r $HOME/Documents/customize/Colloid/Colloid-Dark/gtk-4.0/* $HOME/.config/gtk-4.0
+
+gnome-session-quit
